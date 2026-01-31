54d43a4c24098ff2bc947c084074aaae1530b478
Giox <sandinowork16@gmail.com>
2026-01-31 14:18:03 -0600

    fix: update new modification

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Buenos dias a Todos';
+    return 'Buenas tardes, NestJS!';
   }
 }
